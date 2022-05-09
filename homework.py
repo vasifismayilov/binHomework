8318692cfe7bd1b0263cd7802c10c2f8bfe6c403
vasifismayilov <ismayilov2265@gmail.com>
2022-05-09 23:13:45 +0400

    Homework26

---
+++ homework.py
@@ -0,0 +1,33 @@
+# 1 Istifadəçinin girdiyi mətnlərdəki hərfləri əlifba sırsanıda özünən sonra gələn hərflə dəyişdirən program yazın.
+
+# text = input('Bir cumle yazin: ')
+# 
+# final = []
+# 
+# letters = ''
+
+# for i in text:
+#     if ord(i) >= 65:
+#         new_i = chr(ord(i)+1)
+#         letters += new_i
+#     else:
+#         final.append(letters)
+#         letters = ''
+# else:
+#     final.append(letters)        
+# 
+# print(final)   
+
+# 2 İstifadəçinin girdiyi mətn daxilindəki hərflərin ingilis əlifbasındakı sırasına qarşılıq gələn ədədlərlə dəyişdirilmiş şəkildə göstərin.
+
+
+# text = input('Bir cumle yazin: ')
+# 
+# final = []
+# 
+# for i in text:
+#     if ord(i) >= 65:
+#         new_i = ord(i)
+#         final.append(new_i)
+# 
+# print(*final)        
